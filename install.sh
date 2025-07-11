72cb9deac9d9dca1223942e72a8fcdaf4c77f60e
yiannis ioannides <154235768+yioannides@users.noreply.github.com>
2025-07-11 16:26:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,4 +35,4 @@ if ! grep -Fxq "$ALIAS_CMD" "$SHELL_RC" 2>/dev/null; then
   echo "$ALIAS_CMD" >> "$SHELL_RC"
 fi
 
-echo -e "\nInstallation complete! You can now run the script by typing:\n\033[1m$APP\033[22m (add \033[3mhelp\033[23m for usage instructions)"
+echo -e "\nInstallation complete! You can now run the script by typing:\n\033[1m$APP\033[22m (add \033[3mhelp\033[23m for usage instructions)\n"
